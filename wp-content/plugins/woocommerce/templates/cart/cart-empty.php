11fb6d446395997ffcbf723d0d4ec42df3b50b16
William Kunitake <wykunitake@Williams-MacBook-Pro.local>
2018-08-03 00:47:08 -0300

    Ajustes na Etapa 13

--- wp-content/plugins/woocommerce/templates/cart/cart-empty.php
+++ wp-content/plugins/woocommerce/templates/cart/cart-empty.php
@@ -29,8 +29,6 @@ do_action( 'woocommerce_cart_is_empty' );
 
 if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
 	<p class="return-to-shop">
-		<a class="button wc-backward" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
-			<?php _e( 'Return to shop', 'woocommerce' ) ?>
-		</a>
+		<a class="button wc-backward" href="/produtos">Voltar para a Página de Produtos</a>
 	</p>
 <?php endif; ?>
